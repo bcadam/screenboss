6dd25b5b4574c57bb0cff1073e05393dc5203707
Adam Cragg <adam2@MacBook-Pro-2.local>
2016-02-17 01:21:19 -0500

    added explanation on finding calendar id

--- app/scripts/pages/CalendarsPage.js
+++ app/scripts/pages/CalendarsPage.js
@@ -5,9 +5,27 @@ var ScreenDisplay = require('../components/ScreenDisplay.js');
 var CalendarList = require('../components/CalendarList.js');
 var NewCalendarForm = require('../components/NewCalendarForm.js');
 
-
+import Dialog from 'material-ui/lib/dialog';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import FlatButton from 'material-ui/lib/flat-button';
+import CardText from 'material-ui/lib/card/card-text';
 
 var ScreenListPage = React.createClass({
+    getInitialState:function() {
+        return {
+            open:false
+        };
+    },
+    handleOpen: function(){
+        this.setState({open: true});
+      },
+
+    handleClose: function() {
+        this.setState({open: false});
+    },
     componentWillMount: function(){
 
             var currentUser = Parse.User.current();
@@ -21,17 +39,68 @@ var ScreenListPage = React.createClass({
     render: function() {
         // Render the text of each comment as a list item
         var self = this;
+        var actions = [
+          <FlatButton
+            label="Ok"
+            secondary={true}
+            keyboardFocused={true}
+            onTouchTap={this.handleClose}
+          />,
+        ];
+
         return (
             <div className="col-xs-12">
 
                 <div className="col-xs-12 col-sm-3">
                     <NewCalendarForm />
+                    <RaisedButton fullWidht={true} label="How do I find the calendar id?" onTouchTap={this.handleOpen} />
                 </div>
 
                 <div className="col-xs-12 col-sm-9">
                     <CalendarList />
                 </div>
+                <Dialog
+                  title="How to add a calendar"
+                  actions={actions}
+                  modal={false}
+                  open={this.state.open}
+                  onRequestClose={this.handleClose}
+                >
 
+                <Card>
+                    <CardHeader
+                      title="Adding your own calendar"
+                      actAsExpander={true}
+                      showExpandableButton={true}
+                    />
+                    <CardText expandable={true}>
+                      <ul>
+                        <li>Go to <a href="http://calendar.google.com">Google Calendar</a></li>
+                        <li>Under 'My Calendars', click on the down arrow next to the calendar you want to show</li>
+                        <li>Click 'Settings'</li>
+                        <li>Under 'Calendar address' copy the calendar id (Calendar ID: <b>example@gmail.com</b>)</li>
+                        <li>Click 'Share this calendar' and make the calendar public</li>
+                      </ul>
+                    </CardText>
+                </Card><br />
+                <Card>
+                    <CardHeader
+                      title="Adding a public calendar"
+                      actAsExpander={true}
+                      showExpandableButton={true}
+                    />
+                    <CardText expandable={true}>
+                      <ul>
+                        <li>Go to <a href="http://calendar.google.com">Google Calendar</a></li>
+                        <li>Under 'Other calendars', click on the down arrow next to the calendar you want to show</li>
+                        <li>Click 'Settings'</li>
+                        <li>Under 'Calendar address' copy the calendar id (Calendar ID: <b>example@gmail.com</b>)</li>
+                      </ul>
+                    </CardText>
+                </Card>
+
+
+                </Dialog>
             </div>
         );
     }
